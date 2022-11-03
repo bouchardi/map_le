a04332c605a5959eef01329204ced5082869c582
Isabelle Bouchard <isabelle.bouchard77@gmail.com>
2022-11-02 21:41:08 -0400

    reading rasters and shapefiles

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import geopandas
+import rasterio
+from pathlib import Path
+
+
+def _read_raster(filename):
+    with rasterio.open(filename) as src:
+        data = src.read(1)
+
+    return data
 
 
 def main():
-    print("patate")
+    data_path = Path("data/")
+
+    data_2011_2040 = _read_raster(filename=data_path / "rasters" / "arcp8510000532011-2040.asc")
+    data_2041_2070 = _read_raster(filename=data_path / "rasters" / "arcp8510000532041-2070.asc")
+    data_2071_2100 = _read_raster(filename=data_path / "rasters" / "arcp8510000532071-2100.asc")
+
+    boundaries = geopandas.read_file(data_path / "lcar000b21a_e.shp")
+    boundaries.plot()
 
 
 if __name__ == '__main__':
