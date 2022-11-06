d5f772cf1d54dc02322eb9c8ebdabbc3d2145701
Isabelle Bouchard <isabelle.bouchard77@gmail.com>
2022-11-06 17:18:39 -0500

    analysisis with geopandas and rasterio

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import rasterio
 from pathlib import Path
 
 
-def _read_raster(filename):
+def read_raster(filename):
     with rasterio.open(filename) as src:
         data = src.read(1)
 
@@ -13,9 +13,9 @@ def _read_raster(filename):
 def main():
     data_path = Path("data/")
 
-    data_2011_2040 = _read_raster(filename=data_path / "rasters" / "arcp8510000532011-2040.asc")
-    data_2041_2070 = _read_raster(filename=data_path / "rasters" / "arcp8510000532041-2070.asc")
-    data_2071_2100 = _read_raster(filename=data_path / "rasters" / "arcp8510000532071-2100.asc")
+    data_2011_2040 = read_raster(filename=data_path / "rasters" / "arcp8510000532011-2040.asc")
+    data_2041_2070 = read_raster(filename=data_path / "rasters" / "arcp8510000532041-2070.asc")
+    data_2071_2100 = read_raster(filename=data_path / "rasters" / "arcp8510000532071-2100.asc")
 
     boundaries = geopandas.read_file(data_path / "lcar000b21a_e.shp")
     boundaries.plot()
