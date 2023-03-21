1517c6f01cb7c256b51a596ffb096cc185631ddb
Isabelle Bouchard <isabelle.bouchard@chrysalabs.com>
2023-03-20 23:15:14 -0400

    fix projections

--- utils.py
+++ utils.py
@@ -2,10 +2,7 @@ import numpy as np
 
 import geopandas
 from rasterio.windows import Window
-import rasterio
 from rasterio.features import shapes
-from rasterio.warp import reproject, calculate_default_transform
-from rasterio.enums import Resampling
 from rasterstats import zonal_stats
 
 
@@ -15,7 +12,6 @@ def zonal_stats_for_value(
         value=None,
         data_value=None,
         stats="count",
-        resolution=1
 ):
     raster_data = raster.read(1)
 
@@ -24,7 +20,7 @@ def zonal_stats_for_value(
         raster_data[raster_data != data_value] = raster.nodata
 
     z_stats = [
-        s[stats] * resolution ** 2 for s in
+        s[stats] * (raster.res[0] / 1000) ** 2 for s in
         zonal_stats(
             vectors=vectors,
             raster=raster_data,
@@ -38,7 +34,7 @@ def zonal_stats_for_value(
 
 
 def read_raster(filename, crs):
-    reprojected_filename = filename.parent / f"{filename.stem}_reprojected{filename.suffix}"
+    reprojected_filename = filename.parent / f"{filename.stem}_reprojected_{crs}{filename.suffix}"
     reproject_raster(filename, reprojected_filename, new_crs=crs)
 
     raster = rasterio.open(reprojected_filename)
@@ -111,11 +107,11 @@ def zonal_stats_intersection(
         vectors,
         raster1,
         raster2,
-        affine,
         data_values,
+        affine,
+        resolution,
         stats="count",
         nodata_value=-999,
-        resolution=1
 ):
     new_raster = np.ones(raster1.shape) * nodata_value
     new_raster[np.where(
@@ -135,37 +131,8 @@ def zonal_stats_intersection(
     return z_stats, new_raster
 
 
-def zonal_stats_intersection_gain(
-        vectors,
-        raster1,
-        raster2,
-        affine,
-        data_values,
-        stats="count",
-        nodata_value=-999,
-        resolution=1
-):
-    new_raster = np.ones(raster1.shape) * nodata_value
-    new_raster[np.where(
-        (np.invert(np.isin(raster1, data_values))) &
-        (np.isin(raster2, data_values)))
-    ] = 1
-
-    z_stats = [
-        s[stats] * resolution ** 2 for s in zonal_stats(
-            vectors=vectors,
-            raster=new_raster,
-            affine=affine,
-            nodata=nodata_value,
-            stats=stats
-        )
-    ]
-
-    return z_stats, new_raster
-
-
 def raster_to_vector(filename, crs):
-    reprojected_filename = filename.parent / f"{filename.stem}_reprojected{filename.suffix}"
+    reprojected_filename = filename.parent / f"{filename.stem}_reprojected_{crs}{filename.suffix}"
     reproject_raster(filename, reprojected_filename, new_crs=crs)
 
     mask = None
@@ -181,3 +148,52 @@ def raster_to_vector(filename, crs):
     gdf = geopandas.GeoDataFrame.from_features(results)
 
     return gdf
+
+
+from rasterio.warp import reproject, Resampling, calculate_default_transform
+import rasterio
+
+
+def coregister(infile, match, outfile):
+    """Reproject a file to match the shape and projection of existing raster.
+
+    Parameters
+    ----------
+    infile : (string) path to input file to reproject
+    match : (string) path to raster with desired shape and projection
+    outfile : (string) path to output file tif
+    """
+    # open input
+    with rasterio.open(infile) as src:
+        # open input to match
+        with rasterio.open(match) as match:
+            dst_crs = match.crs
+
+            # calculate the output transform matrix
+            dst_transform, dst_width, dst_height = calculate_default_transform(
+                src.crs,  # input CRS
+                dst_crs,  # output CRS
+                match.width,  # input width
+                match.height,  # input height
+                *match.bounds,  # unpacks input outer boundaries (left, bottom, right, top)
+            )
+
+        # set properties for output
+        dst_kwargs = src.meta.copy()
+        dst_kwargs.update({"crs": dst_crs,
+                           "transform": dst_transform,
+                           "width": dst_width,
+                           "height": dst_height,
+                           "nodata": src.nodata})
+        # open output
+        with rasterio.open(outfile, "w", **dst_kwargs) as dst:
+            # iterate through bands and write using reproject function
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=dst_transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
